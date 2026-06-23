ce69da5e6b1e4ee09b6dbe295c59d204610be17b
Walter Teixeira <kassambinha@email.com>
2026-06-23 18:46:20 -0300

    Dashboard principal AgroClima Cafe funcional

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
 
-# Create your views here.
+from municipios.models import Municipio
+from clima.models import Clima
+from geadas.models import Geada
+
+from django.db.models import Avg
+
+
+def dashboard_home(request):
+
+    total_municipios = Municipio.objects.count()
+
+    total_climas = Clima.objects.count()
+
+    total_geadas = Geada.objects.count()
+
+    temperatura_media = (
+        Clima.objects.aggregate(
+            media=Avg('temperatura')
+        )['media']
+    )
+
+    context = {
+
+        'total_municipios': total_municipios,
+
+        'total_climas': total_climas,
+
+        'total_geadas': total_geadas,
+
+        'temperatura_media': round(
+            temperatura_media,
+            2
+        ) if temperatura_media else 0
+    }
+
+    return render(
+        request,
+        'dashboard/home.html',
+        context
+    )
